8512529e2306a830e60429c0610eda40b834133a
eliagbayani <eli@eol.org>
2016-01-05 03:31:29 -0500

    embed video

---
+++ extensions/EmbedVideo/EmbedVideo.i18n.magic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * EmbedVideo
+ * EmbedVideo Magic Words
+ *
+ * @license		MIT
+ * @package		EmbedVideo
+ * @link		https://www.mediawiki.org/wiki/Extension:EmbedVideo
+ *
+ **/
+
+
+/* commented by Eli
+$magicWords = [];
+$magicWords['en']  = [
+	'ev'	=> [0, 'ev'],
+	'evp'	=> [0, 'evp'],
+	'evt'	=> [0, 'evt']
+];
+*/
+
+//added by Eli
+$magicWords = array();
+$magicWords['en']  = array(
+	'ev'	=> array(0, 'ev'),
+	'evp'	=> array(0, 'evp'),
+	'evt'	=> array(0, 'evt')
+);
+
+?>
