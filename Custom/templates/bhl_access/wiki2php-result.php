dcb7eb9cca9f2d8f5f135642561c4e4f5e317d26
eliagbayani <eli@eol.org>
2016-06-27 04:02:54 -0400

    temp debug

--- Custom/templates/bhl_access/wiki2php-result.php
+++ Custom/templates/bhl_access/wiki2php-result.php
@@ -2,7 +2,14 @@
 // namespace php_active_record;
     /* 
         Expects: $params
+        Array
+        (
+            [search_type] => wiki2php
+            [wiki_title] => 16194405 ee667ff5a5361aedaaa35b2e1e55338e
+            [overwrite] => 1
+        )
     */
+    echo "<pre>"; print_r($params); echo "</pre>";
     $wiki_text = self::get_wiki_text($params['wiki_title']);
     self::parse_wiki_text($wiki_text, $params);
 ?>
