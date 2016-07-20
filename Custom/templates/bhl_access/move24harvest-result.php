6210954869e09ab0a9f9cb99a01fca47b6b6a4c8
eliagbayani <eli@eol.org>
2016-07-20 06:55:22 -0400

    formatting

--- Custom/templates/bhl_access/move24harvest-result.php
+++ Custom/templates/bhl_access/move24harvest-result.php
@@ -18,33 +18,26 @@
     <h3>Move to "<?php echo $str ?>"</h3>
     <div>
     <?php
-    
     if($params['token'] = self::get_move_token($params['wiki_title']))
     {
         $arr = self::move_file($params);
-        echo "<pre>222"; print_r($arr); echo "</pre>";
+        // echo "<pre>222"; print_r($arr); echo "</pre>"; //debug
+
+        if($msg = $arr['error']['code']) self::display_message(array('type' => "error", 'msg' => $msg));
+        if($msg = $arr['error']['info']) self::display_message(array('type' => "error", 'msg' => $msg));
+        
         if($new_title = @$arr['move']['to'])
         {
-            
             $wiki_page = "../../wiki/" . $new_title;
-
             ?>
             <script type="text/javascript">
             location.href = '<?php echo $wiki_page ?>';
             </script>
             <?php
-            
-            
         }
     }
-    else
-    {
-        self::display_message(array('type' => "error", 'msg' => "Move failed."));
-    }
-    
+    else self::display_message(array('type' => "error", 'msg' => "Move failed."));
     ?>
     </div>
 </div>
-    
-    
 
