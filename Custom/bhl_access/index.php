74c4f6c37f9356d6e4b863f34364d59e69c3975f
eliagbayani <eli@eol.org>
2016-08-02 21:05:27 -0400

    projects 4

--- Custom/bhl_access/index.php
+++ Custom/bhl_access/index.php
@@ -72,6 +72,8 @@ if(isset($params['search_type']))
     elseif($params['search_type'] == "move2wiki")     print $ctrler->render_template('move2wiki-result', array('params' => @$params));
     elseif($params['search_type'] == "reviewexcerpt") print $ctrler->render_template('reviewexcerpt-result', array('params' => @$params));
     elseif($params['search_type'] == "wiki2php")      print $ctrler->render_template('wiki2php-result', array('params' => @$params));
+    elseif($params['search_type'] == "wiki2php_project")      print $ctrler->render_template('wiki2php_project-result', array('params' => @$params));
+    
     elseif($params['search_type'] == "gen_archive")   print $ctrler->render_template('gen_archive-result', array('params' => @$params));
     elseif($params['search_type'] == "gen_archive_all") print $ctrler->render_template('gen_archive_all-result', array('params' => @$params));
     elseif($params['search_type'] == "move24harvest") print $ctrler->render_template('move24harvest-result', array('params' => @$params));
