ae5a3f147f7f0014e1c3ba68aefacebc1aa70bd0
eliagbayani <eli@eol.org>
2016-08-08 07:07:25 -0400

    implement My Projects

--- Custom/templates/bhl_access/projectsmenu-result.php
+++ Custom/templates/bhl_access/projectsmenu-result.php
@@ -5,10 +5,10 @@
 switch ($params['radio']) {
     case "proj_my":     $str = "My projects"; break;
     case "proj_active": $str = "Active projects"; break;
-    case "proj_comp": $str = "Completed projects"; break;
-    case "proj_start": $str = "Start a new project"; break;
+    case "proj_comp":   $str = "Completed projects"; break;
+    case "proj_start":  $str = "Start a new project"; break;
     // default:
-    //     code to be executed if n is different from all labels;
+    // code to be executed if n is different from all labels;
 }
 
 if($params['radio'] == "proj_active")
@@ -23,13 +23,23 @@ elseif($params['radio'] == "proj_comp")
     $rows = self::list_titles_by_type('completed', false, true); //2nd param is book_title, 3rd param is boolean $projects
     $str .= " = " . count($rows);
 }
-
+elseif($params['radio'] == "proj_my")
+{
+    // [http://editors.eol.localhost/LiteratureEditor/wiki/User:EAgbayani Eli E. Agbayani]
+    if(preg_match("/User:(.*?) /ims", $this->compiler, $arr))
+    {
+        $username = $arr[1];
+        echo "<br>username is: [$username]<br>";
+        $rows = self::list_titles_by_type('all_projects', false, true, $username); //2nd param is book_title, 3rd param is boolean $projects, 4th param is logged-in username
+        $str .= " = " . count($rows);
+    }
+}
 ?>
 <div id="accordion_open">
     <h3><?php echo $str ?></h3>
     <div>
         <?php
-        // echo "<pre>xxx"; print_r($params); echo "yyy</pre>";
+        // echo "<pre>"; print_r($params); echo "</pre>";
         if($params['radio'] == "proj_start")
         {
             if(isset($params['proj_name']) && !isset($params['overwrite']))
@@ -42,19 +52,15 @@ elseif($params['radio'] == "proj_comp")
                 require_once("../templates/bhl_access/proj-start-form.php"); //print self::render_layout(@$params, 'proj-start-form')
             }
         }
-        
-        
-        if(in_array($params['radio'], array("proj_active", "proj_comp")))
+        elseif(in_array($params['radio'], array("proj_active", "proj_comp", "proj_my")))
         {
             if($rows)
             {
                 // echo "<pre>"; print_r($rows); echo "</pre>";
-                
                 $data = array('group' => 'projects', 'records' => $rows);
                 print self::render_template('article-table', array('data' => @$data));
             }
         }
-
         ?>
     </div>
 </div>
