abe8fc3d8205f79530a37ebe5bbede7d72367380
eliagbayani <eli@eol.org>
2018-01-22 03:13:26 -0500

    formatSizeUnits()

--- Custom/lib/Functions.php
+++ Custom/lib/Functions.php
@@ -147,6 +147,16 @@ class Functions
         }
     }
     
+    public static function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824) $bytes = number_format($bytes / 1073741824, 1) . ' GB';
+        elseif ($bytes >= 1048576) $bytes = number_format($bytes / 1048576, 1) . ' MB';
+        elseif ($bytes >= 1024) $bytes = number_format($bytes / 1024, 1) . ' KB';
+        elseif ($bytes > 1) $bytes = $bytes . ' bytes';
+        elseif ($bytes == 1) $bytes = $bytes . ' byte';
+        else $bytes = '0 bytes';
+        return $bytes;
+    }
     public static function lookup_with_cache($url, $options = array())
     {
         // default expire time is 30 days
